5296c2ecdb027ceb935d7556fcfe7ad976e538f6
myoungin <myoungin729@gmail.com>
2023-07-18 00:57:27 +0900

    Feat: message entity - setter 제거

--- server/momo/src/main/java/com/momo/chat/entity/Message.java
+++ server/momo/src/main/java/com/momo/chat/entity/Message.java
@@ -4,12 +4,10 @@ import com.momo.member.entity.Member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 @Getter
-@Setter
 @Entity
 @NoArgsConstructor
 public class Message {
